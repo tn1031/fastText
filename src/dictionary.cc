18b331a838c9c15fd2432e8f1b030f00560f8131
tn1031 <ttt.nakamura1031@gmail.com>
2020-10-15 14:54:39 +0900

    skip unknown word

--- src/dictionary.cc
+++ src/dictionary.cc
@@ -60,6 +60,9 @@ int32_t Dictionary::find(const std::string& w, uint32_t h) const {
 }
 
 void Dictionary::add(const std::string& w) {
+  if (!word2sideinfo_.count(w)) {
+    return;
+  }
   int32_t h = find(w);
   ntokens_++;
   if (word2int_[h] == -1) {
